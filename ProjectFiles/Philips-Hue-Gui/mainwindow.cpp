99bf3d79e5a35fdb8153ae3b827c01f7e1f12710
BOB450 <bwvoskuil@gmail.com>
2021-11-15 09:24:10 -0700

    added library and started on stuff

--- ProjectFiles/Philips-Hue-Gui/mainwindow.cpp
+++ ProjectFiles/Philips-Hue-Gui/mainwindow.cpp
@@ -1,15 +1,68 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <include/hueplusplus/Bridge.h>
+#include <algorithm>
+#include <iostream>
+#include <include/hueplusplus/LinHttpHandler.h>
+using namespace std;
+using namespace hueplusplus;
+namespace hue = hueplusplus;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+
+    // Configure existing connections here, or leave empty for new connection
+    const std::string macAddress = "";
+    const std::string username = "";
+
+    // Connects to a bridge and returns it.
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+hue::Bridge connectToBridge()
+{
+    hue::BridgeFinder finder(std::make_shared<SystemHttpHandler>());
+
+    std::vector<hue::BridgeFinder::BridgeIdentification> bridges = finder.findBridges();
+
+    for (const auto& bridge : bridges)
+    {
+        std::cout << "Bridge: " << bridge.mac << " at " << bridge.ip << '\n';
+    }
+    if (bridges.empty())
+    {
+        std::cout << "Found no bridges\n";
+        throw std::runtime_error("no bridges found");
+    }
+
+    if (macAddress.empty())
+    {
+        std::cout << "No bridge given, connecting to first one.\n";
+        return finder.getBridge(bridges.front());
+    }
+    if (!username.empty())
+    {
+        finder.addUsername(macAddress, username);
+    }
+    auto it = std::find_if(
+        bridges.begin(), bridges.end(), [&](const auto& identification) { return identification.mac == macAddress; });
+    if (it == bridges.end())
+    {
+        std::cout << "Given bridge not found\n";
+        throw std::runtime_error("bridge not found");
+    }
+    return finder.getBridge(*it);
+}
+
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Philpis-Hue-Desktop-Gui
+# Philips-Hue-Desktop-Gui
+
 A cross platform application to control your Philips hue lights
+Open source mit license
