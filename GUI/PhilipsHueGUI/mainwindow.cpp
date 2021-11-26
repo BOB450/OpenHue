64b764a137e41e77964ae25335bf7e65bd9edb7e
BOB450 <bwvoskuil@gmail.com>
2021-11-26 16:22:46 -0700

    added includes for hueplusplus

--- GUI/PhilipsHueGUI/mainwindow.cpp
+++ GUI/PhilipsHueGUI/mainwindow.cpp
@@ -1,6 +1,27 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <thread>
+
+#include <hueplusplus/Bridge.h>
+#include <hueplusplus/BaseDevice.h>
+#include <hueplusplus/Light.h>
+
+#ifdef _MSC_VER
+#include <hueplusplus/WinHttpHandler.h>
+
+using SystemHttpHandler = hueplusplus::WinHttpHandler;
+
+#else
+#include <hueplusplus/LinHttpHandler.h>
+
+using SystemHttpHandler = hueplusplus::LinHttpHandler;
+
+#endif
+
+namespace hue = hueplusplus;
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,5 +32,11 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+
 }
 
