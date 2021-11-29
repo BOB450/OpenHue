56531274da1094227706d01528f00a44d905c863
BOB450 <bwvoskuil@gmail.com>
2021-11-28 23:02:57 -0700

    added licence notice to each file as gnu gpl 3 licence recommends

--- GUI/PhilipsHueGUI/mainwindow.h
+++ GUI/PhilipsHueGUI/mainwindow.h
@@ -1,3 +1,21 @@
+/**
+    \file mainwindow.h
+    Copyright Notice\n
+    Copyright (C) 2021  BOB450 - developer\n
+    This file is part of PhilipsHueGUI.
+    PhilipsHueGUI is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    PhilipsHueGUI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with PhilipsHueGUI.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
