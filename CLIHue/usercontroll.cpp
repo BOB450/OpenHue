b50f57960cd2abed01460685494fa3d41b22209f
BOB450 <bwvoskuil@gmail.com>
2021-11-29 09:59:48 -0700

    updated name

--- CLIHue/usercontroll.cpp
+++ CLIHue/usercontroll.cpp
@@ -2,17 +2,17 @@
     \file usercontroll.cpp
     Copyright Notice\n
     Copyright (C) 2021  BOB450 - developer\n
-    This file is part of PhilipsHueGUI.
-    PhilipsHueGUI is free software: you can redistribute it and/or modify
+    This file is part of OpenHue.
+    OpenHue is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    PhilipsHueGUI is distributed in the hope that it will be useful,
+    OpenHue is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU Lesser General Public License for more details.
     You should have received a copy of the GNU Lesser General Public License
-    along with PhilipsHueGUI.  If not, see <http://www.gnu.org/licenses/>.
+    along with OpenHue.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
 
